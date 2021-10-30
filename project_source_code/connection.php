7cd5bf867aef3279f705c226412be6902a00f840
yashasc2k1 <93425785+yashasc2k1@users.noreply.github.com>
2021-10-30 09:36:26 +0000

    Add files via upload

---
+++ project_source_code/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "Password123#@!";
+$dbname = "Patient";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+>
